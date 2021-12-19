4e078016828b73b9079a3dca08d1cf5d8fff85fc
Amrata Pathak <amrata3003@gmail.com>
2021-12-19 18:46:12 +0530

    Added header layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require('./hello_react')
 require('bootstrap')
 import 'bootstrap/dist/css/bootstrap'
 // Uncomment to copy all static images under ../images to the output folder and reference
