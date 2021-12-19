80a7b6d23f42dbada1b83d145746ad405d4da6c6
Amrata Pathak <amrata3003@gmail.com>
2021-12-19 19:11:27 +0530

    Different Homepage for authoized and unauthorized users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  authenticated :user do
-    root "pages#my_todo_items", as: :authenticated_root
+  devise_scope :user do
+    unauthenticated do
+      root "devise/sessions#new"
+    end
+    authenticated :user do
+      root "pages#my_todo_items", as: :authenticated_root
+    end
   end
-  root 'pages#home'
 end
