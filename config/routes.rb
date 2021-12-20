d9fcf4ef3c1a864a38374d5e03e9441e28fe2d0f
Amrata Pathak <amrata3003@gmail.com>
2021-12-20 13:49:24 +0530

    Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,10 @@ Rails.application.routes.draw do
       root "pages#my_todo_items", as: :authenticated_root
     end
   end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :todo_items, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
 end
