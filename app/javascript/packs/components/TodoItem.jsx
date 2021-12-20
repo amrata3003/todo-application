01020e0b8ccbbd0e62d113e87d831d0b5d210fdc
Amrata Pathak <amrata3003@gmail.com>
2021-12-20 19:10:33 +0530

    Added lodash to debounce requests

--- app/javascript/packs/components/TodoItem.jsx
+++ app/javascript/packs/components/TodoItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import _ from "lodash";
 import axios from "axios";
 import setAxiosHeaders from "./AxiosHeaders";
 
@@ -18,11 +19,13 @@ class TodoItem extends React.Component {
   }
 
   handleChange() {
+    this.setState({
+      complete: this.completedRef.current.checked
+    });
     this.updateTodoItem();
   }
 
-  updateTodoItem() {
-    this.setState({ complete: this.completedRef.current.checked });
+  updateTodoItem = _.debounce(() => {
     setAxiosHeaders();
     axios
       .put(this.path, {
@@ -35,7 +38,7 @@ class TodoItem extends React.Component {
       .catch(error => {
         console.log(error);
       });
-  }
+  }, 1000);
 
   handleDestroy() {
     setAxiosHeaders();
